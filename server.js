0876873585f60ba456af3c6a68e7e1cdcc8cb22a
yjung25 <yjung25@myseneca.ca>
2017-10-29 18:42:44 -0400

    1st sever setting

---
+++ server.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var http = require('http');
+var path = require('path');
+
+var app = express();
+
+var router = express.Router();
+
+//Import modules from outside.
+var static = require('serve-static');
+var expressErrorHandler = require('express-error-handler');
+
+//Setting port number for the server.
+app.set('port', process.env.PORT||3000);
+
+//The middleWare for using path to connect to the specific folder.
+app.use('/', static(path.join(__dirname, 'html')));
+
+//Error control.
+var errorHandler = expressErrorHandler({
+    static:{
+        '404':'./html/404.html'
+    }
+});
+
+app.use(expressErrorHandler.httpError(404));
+app.use(errorHandler);
+
+//Making server.
+http.createServer(app).listen(app.get('port'), function(){
+    console.log('Server is waiting requests from clients on port #' + app.get('port'));
+});
