dd989e39fca055e2da71606352f893a06381c123
yjung25 <yjung25@myseneca.ca>
2017-11-14 22:09:13 -0500

    css update(2)

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
     server_port:process.env.PORT || 3000,
-    db_url:"mongodb://localhost:27017/local",
-   // db_url:"mongodb://leedo01219:Jun1452563#@ds249605.mlab.com:49605/heroku_bmbp7spr",
+    //db_url:"mongodb://localhost:27017/local",
+    db_url:"mongodb://leedo01219:Jun1452563#@ds249605.mlab.com:49605/heroku_bmbp7spr",
     db_schemas:[
         {file:"./user_schema",collection:"users",schemaName:"UserSchema",modelName:"UserModel"}
     ],
